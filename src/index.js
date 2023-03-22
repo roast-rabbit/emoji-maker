f83285347b12e346cc700e2f57f268b37c28fe53
roast-rabbit <roastrabbit@protonmail.com>
2023-03-22 13:11:44 +0800

    new

--- src/index.js
+++ src/index.js
@@ -89,11 +89,6 @@ const loadPositionData = async function (positionData) {
 
 loadPositionData(positionData);
 
-// const togglePanBtn = document.querySelector("#toggle-pan");
-// togglePanBtn.addEventListener("click", function () {
-//   toggleMode.call(this, modes.pan);
-// });
-
 let mousePressed = false;
 const setPanEvent = (canvas) => {
   canvas.on("mouse:move", (event) => {
@@ -119,31 +114,6 @@ const setPanEvent = (canvas) => {
 };
 setPanEvent(canvas);
 
-// const toJSONBtn = document.querySelector("#to-json");
-// toJSONBtn.addEventListener("click", () => {
-//   const toBeSaved = JSON.stringify(canvas.toJSON());
-
-//   localStorage.setItem("fabricSaved", toBeSaved);
-// });
-
-// const loadJSONBtn = document.querySelector("#load-json");
-// loadJSONBtn.addEventListener("click", () => {
-//   const fabricSaved = localStorage.getItem("fabricSaved");
-//   canvas.clear();
-//   canvas.loadFromJSON(fabricSaved, () => {
-//     canvas.renderAll.bind(canvas),
-//       // set each objects selection border color property etc.
-//       canvas._objects.forEach((obj) => {
-//         obj.set({
-//           borderColor: "#0c8ce9",
-//           cornerColor: "#0c8ce9",
-//           cornerSize: 10,
-//           transparentCorners: false,
-//         });
-//       });
-//   });
-// });
-
 // Add an event listener to the document object to listen to the delete key pressed (in Windows it's named "Backspace" key, in mac it's named "delete"), and it will delete current selected obejct on canvas
 document.addEventListener("keydown", function (event) {
   const { key } = event;
@@ -165,7 +135,7 @@ export function showCurrentLayerInfo(layerData, newOrder) {
         newOrder?.[index] || index
       } class="draggable p-s" draggable="true">
           <div style="display:flex; align-items: center">
-            <div class="mr_2"><img src="${obejct.toDataURL()}"/></div>
+            <div class="mr_2"><img src="${obejct?.toDataURL()}"/></div>
             <div  class="small text_gray">Layer ${obejcts.length - index}</div>
           </div>
             <img data-label="delete" src="data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E">
